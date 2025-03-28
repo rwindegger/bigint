1fe8495a7168f977bd18aee55d1ca362a3d8bee3
Rene Windegger <rene@windegger.wtf>
2025-03-28 21:01:43 +0100

    Added some unit tests for higher coverage

--- tests/io_tests.cpp
+++ tests/io_tests.cpp
@@ -8,38 +8,59 @@
 
 namespace {
     TEST(bigint, decimal_positive_os_test) {
-        bigint::bigint<128, true> a(123456789);
+        bigint::bigint<128, true> const a(123456789);
         std::ostringstream oss;
         oss << std::dec << a;
         ASSERT_EQ(oss.str(), "123456789");
     }
 
     TEST(bigint, decimal_negative_os_test) {
-        bigint::bigint<128, true> a(-123456789);
+        bigint::bigint<128, true> const a(-123456789);
         std::ostringstream oss;
         oss << std::dec << a;
         ASSERT_EQ(oss.str(), "-123456789");
     }
 
     TEST(bigint, hex_unsigned_os_test) {
-        bigint::bigint<128, false> a(0x1A2B3C4D);
+        bigint::bigint<128, false> const a(0x1A2B3C4D);
         std::ostringstream oss;
         oss << std::hex << std::nouppercase << a;
         ASSERT_EQ(oss.str(), "1a2b3c4d");
     }
 
     TEST(bigint, hex_signed_negative_os_test) {
-        bigint::bigint<128, true> a(-123456789);
+        bigint::bigint<128, true> const a(-123456789);
         std::ostringstream oss;
         oss << std::hex << std::nouppercase << a;
         ASSERT_EQ(oss.str(), "fffffffffffffffffffffff8a432eb");
     }
 
     TEST(bigint, octal_unsigned_os_test) {
-        bigint::bigint<128, false> a(123456);
+        bigint::bigint<128, false> const a(123456);
         std::ostringstream oss;
         oss << std::oct << a;
         ASSERT_EQ(oss.str(), "361100");
+    }
+
+    TEST(bigint, decimal_zero_os_test) {
+        bigint::bigint<128, false> const a(0);
+        std::ostringstream oss;
+        oss << a;
+        ASSERT_EQ(oss.str(), "0");
+    }
+
+    TEST(bigint, octal_zero_os_test) {
+        bigint::bigint<128, false> const a(0);
+        std::ostringstream oss;
+        oss << std::oct << a;
+        ASSERT_EQ(oss.str(), "0");
+    }
+
+    TEST(bigint, hexadecimal_zero_os_test) {
+        bigint::bigint<128, false> const a(0);
+        std::ostringstream oss;
+        oss << std::hex << a;
+        ASSERT_EQ(oss.str(), "00");
     }
 
     TEST(bigint, positive_decimal_is_test) {
@@ -49,14 +70,14 @@ namespace {
         ASSERT_EQ(a, 123456789);
     }
 
-    TEST(bigint, NegativeDecimal) {
+    TEST(bigint, negative_decimal_is_test) {
         std::istringstream iss("-987654321");
         bigint::bigint<128, true> a;
         iss >> a;
         ASSERT_EQ(a, -987654321);
     }
 
-    TEST(bigint, Hexadecimal) {
+    TEST(bigint, hexadecimal_is_test) {
         std::istringstream iss("1a2b3c4d");
         iss >> std::hex;
         bigint::bigint<128, false> a;
@@ -64,11 +85,26 @@ namespace {
         ASSERT_EQ(a, 0x1a2b3c4d);
     }
 
-    TEST(bigint, Octal) {
+    TEST(bigint, octal_is_test) {
         std::istringstream iss("361100");
         iss >> std::oct;
         bigint::bigint<128, false> a;
         iss >> a;
         ASSERT_EQ(a, 0361100);
+    }
+
+    TEST(bigint, empty_steam_is_test) {
+        std::istringstream iss("");
+        bigint::bigint<128, false> a;
+        iss >> a;
+        ASSERT_EQ(a, 0);
+    }
+
+    TEST(bigint, negative_hexadecimal_fails_is_test) {
+        std::istringstream iss("-987654321");
+        iss >> std::hex;
+        bigint::bigint<128, false> a;
+        iss >> a;
+        ASSERT_EQ(a, 0);
     }
 }
